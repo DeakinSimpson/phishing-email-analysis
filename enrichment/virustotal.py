31113733b15116e70578b9cb2f7f332a8d355b71
DeakinSimpson <deakinsimpson976@gmail.com>
2026-05-27 09:52:55 +1000

    Add URL scanning functionality using VirusTotal API

---
+++ enrichment/virustotal.py
@@ -0,0 +1,28 @@
+import requests
+import config
+
+def scanurl(url):
+    endpoint = "https://www.virustotal.com/api/v3/urls"
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/x-www-form-urlencoded",
+        "x-apikey": config.VIRUSTOTAL_API_KEY
+    }
+
+    data = {"url": url}
+    try:
+        response = requests.post(endpoint, headers=headers, data=data)
+
+        analysis_response = requests.get(response.json()["data"]["links"]["self"], headers=headers)
+
+        stats = {
+            "malicious": analysis_response.json()["data"]["attributes"]["stats"]["malicious"],
+            "suspicious": analysis_response.json()["data"]["attributes"]["stats"]["suspicious"]
+        }
+
+        return stats
+    except:
+        return None
+    
+    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import analyser
-from enrichment import domainchecker
+from enrichment import domainchecker, virustotal
 from parsers import csvpasser
 
 path = "phishing-email-analysis/data/CEAS_08.csv"
@@ -8,4 +8,5 @@ date = "2008-01-01"
 result = csvpasser.importcsv(path)
 testurl = result[4]["urls"][0]
 
-print(domainchecker.getage(domainchecker.getdomaininfo(testurl), date))
+# print(domainchecker.getage(domainchecker.getdomaininfo(testurl), date))
+print(virustotal.scanurl("https://www.google.com"))
